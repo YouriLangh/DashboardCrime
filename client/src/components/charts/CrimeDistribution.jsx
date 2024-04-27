fed2a5a8792d072121fd0287589a0e1141ad185d
YouriLangh <neere210@hotmail.com>
2024-04-27 21:21:31 +0200

    scrollbars re-added for visuals

--- client/src/components/charts/CrimeDistribution.jsx
+++ client/src/components/charts/CrimeDistribution.jsx
@@ -93,7 +93,7 @@ function CrimeDistribution({ filters, filterCallback }) {
       style={{
         width: "100%",
         height: "100%",
-        overflowY: "scroll",
+        overflowY: "auto",
         position: "relative",
       }}
       onScroll={handleScroll}
